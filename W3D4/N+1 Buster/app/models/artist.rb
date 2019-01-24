357cec820d1a630962ab68ad73a0df0e02aeb4d8
Ivan Kwong <ivan.kwong710@gmail.com>
2019-01-23 23:00:04 -0800

    Practice Active_Record exercises and N+1 queries

---
+++ W3D4/N+1 Buster/app/models/artist.rb
@@ -0,0 +1,32 @@
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  #Count all of the tracks on each album by a given artist.
+  def better_tracks_query
+    albums = self 
+      .albums
+      .select('albums.*, COUNT(*) AS tracks_count')
+      .joins(:tracks)
+      .group('albums_id')
+
+    albums_counts = {}
+    albums.each do |album|
+      album_counts[album.title] = album.tracks_count
+    end
+
+    album_counts
+  end
+end
---
+++ W3D4/N+1 Buster/app/models/house.rb
@@ -0,0 +1,32 @@
+class House < ApplicationRecord
+  has_many :gardeners,
+    class_name: 'Gardener',
+    foreign_key: :house_id,
+    primary_key: :id
+
+  has_many :plants,
+    through: :gardeners,
+    source: :plants
+
+  def n_plus_one_seeds
+    plants = self.plants
+    seeds = []
+    plants.each do |plant|
+      seeds << plant.seeds
+    end
+
+    seeds
+  end
+
+  #Create an array of all the seeds within a given house.
+  def better_seeds_query
+    plants = self.plants.include(:seeds) #prefetch seeds
+    seeds = []
+
+    plants.each do |plant|
+      seeds << plant.seeds #will not fire a query for each plant
+    end
+
+    seeds
+  end
+end
